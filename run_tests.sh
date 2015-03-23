0cbd672ae3d0070fe127a44cc6cab13eaabd2fae
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2015-03-23 13:18:35 +0000

    Travis: Better script

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Maybe there's a better way to do this
+set -e
+
 mkdir tmp
 cd tmp
 
@@ -12,11 +15,12 @@ case $1 in
         mkdir pkg
         cd pkg
         wget ftp://ftp.gap-system.org/pub/gap/gap47/tar.gz/packages/GAPDoc-1.5.1.tar.gz
-        tar xvzf GAPDoc-1.5.1.tar.gz > /dev/null
-        ln -s ../.. io
+        tar xvzf GAPDoc-1.5.1.tar.gz 2> /dev/null
+        ln -s ../../.. io
         cd io
         ./configure
         make
+        cd ../..
         ;;
     hpcgap)
         git clone --depth=50 https://github.com/gap-system/gap.git gap
@@ -24,10 +28,11 @@ case $1 in
         git checkout hpcgap-default
         ./make.hpc
         cd pkg
-        ln -s ../.. io
+        ln -s ../../.. io
         cd io
         ./configure CFLAGS="`cat ../../build/cflags`"
         make
+        cd ../..
     ;;
 esac
 echo "Read(\"tst/testall.g\"); quit;" | sh bin/gap.sh | tee testlog.txt | grep --colour=always -E "########> Diff|$"
